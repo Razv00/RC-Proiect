833be27f00061dc0f12c5e2b5afffa142b330975
VecliucTeo <84568091+VecliucTeo@users.noreply.github.com>
2021-12-03 09:23:40 +0200

    Update DecodePacket.py

--- DecodePacket.py
+++ DecodePacket.py
@@ -3,95 +3,95 @@ class DecodeConnect(ABC):
     def __init__(self):
         pass
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+       
         pass
 class DecodeConnaback(ABC):
     def __init__(self):
         pass
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodePUBLISH(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+       
         pass
 class DecodePUBACK(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodePUBREC(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodePUBREL(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+       
         pass
 class DecodePUBCOMP(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodeSUBSCRIBE(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodeSUBACK(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodeUNSUBSCRIBE(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+       
         pass
 class DecodeUNSUBACK(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+       
         pass
 class DecodePINGREQ(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+       
         pass
 class DecodePINGRESP(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
 class DecodeDISCONNECT(ABC):
     def __init__(self):
         pass
 
     def decodeElem(self):
-        # to do ma gandesc ca ar trebui sa iau pachetul facut in AllPackets si sa decodic valorile pe care le am
+        
         pass
